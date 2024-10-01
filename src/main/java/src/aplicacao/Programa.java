e60e78733172687de7faeb41a412d79350ac26ff
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-10-01 20:25:55 -0300

    adicionando usuario

--- src/main/java/src/aplicacao/Programa.java
+++ src/main/java/src/aplicacao/Programa.java
@@ -2,12 +2,26 @@ package src.aplicacao;
 
 import src.dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Programa {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Carlos da silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Carlos da silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Pronto!");
+
 
         System.out.println(p1);
         System.out.println(p2);
